ac90fec1a1648bb0f28f17df153fbdd8457e2dc9
harman1390 <harmansingh91677@gmail.com>
2022-11-09 17:53:07 -0500

    Lab 1

--- Full-Stack-2/Lab2/Exercise-2/emit.js
+++
@@ -1,9 +0,0 @@
-var events = require("events");
-var eventEmitter = new events.EventEmitter();
-
-var fn = function () {
-  console.log("call me.");
-};
-
-eventEmitter.on("call", fn);
-eventEmitter.emit("call");
